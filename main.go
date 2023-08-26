dd74241f9f5bcaeb4889c8a8b9768b174e27a6f9
Yash Raj <yashraj0207@outlook.com>
2023-08-27 02:00:16 +0530

    Add Gin package and create test routes

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+
+	"github.com/gin-gonic/gin"
+)
+
+var html = template.Must(template.New("https").Parse(`
+<html>
+<head>
+  <title>Https Test</title>
+</head>
+<body>
+  <h1 style="color:red;">Welcome, Ginner!</h1>
+</body>
+</html>
+`))
 
 func main() {
-	fmt.Println("Hello world!")
+	r := gin.Default()
+	r.SetHTMLTemplate(html)
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(200, "https", gin.H{
+			"status": "success",
+		})
+	})
+
+	r.GET("/ping", func(c *gin.Context){
+		c.JSON(200, gin.H{
+			"message": "hello world",
+		})
+	})
+
+	r.Run(":8080")
 }
