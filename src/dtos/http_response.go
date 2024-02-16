8747ce950efce5d82482e4e08ba0e0e5e84b00d5
Vinayak Goyal <73058928+ivinayakg@users.noreply.github.com>
2024-02-17 01:18:48 +0530

    feat: added form meta Dto

--- src/dtos/http_response.go
+++ src/dtos/http_response.go
@@ -1,9 +1,12 @@
 package dtos
 
-import "github.com/Real-Dev-Squad/wisee-backend/src/models"
+import (
+	"time"
+
+	"github.com/Real-Dev-Squad/wisee-backend/src/models"
+)
 
 type ErrorResponse struct {
-	Code    int         `json:"code"`
 	Message string      `json:"message"`
 	Detail  interface{} `json:"detail"`
 }
@@ -15,16 +18,41 @@ type ResponseDto struct {
 }
 
 type CreateUpdateFormResponseDto struct {
-	ID          int64              `json:"id"`
+	Id          int64              `json:"id"`
 	Content     models.FormContent `json:"content"`
 	OwnerId     int64              `json:"owner_id"`
 	CreatedById int64              `json:"created_by_id"`
 	Status      string             `json:"status"`
 	UpdatedById int64              `json:"updated_by_id"`
+	CreatedAt   string             `json:"created_at"`
+	UpdatedAt   string             `json:"updated_at"`
 }
 
 type GetFormMetaDataResponseDto struct {
-	FormId              int64 `json:"form_id"`
-	AcceptingResponses  bool  `json:"accepting_responses"`
-	AllowGuestResponses bool  `json:"allow_guest_responses"`
+	Id                               int64     `json:"id"`
+	FormId                           int64     `json:"form_id"`
+	IsDeleted                        bool      `json:"is_deleted"`
+	AccepctingResponses              bool      `json:"accepting_responses"`
+	AllowGuestResponses              bool      `json:"allow_guest_responses"`
+	AllowMultipleRepsonses           bool      `json:"allow_multiple_responses"`
+	SendConfirmationEmailToRespondee bool      `json:"send_confirmation_email_to_respondee"`
+	SendSubmissionEmailToOwner       bool      `json:"send_submission_email_to_owner"`
+	ValidTill                        time.Time `json:"valid_till"`
+	UpdatedById                      *int64    `json:"updated_by_id"`
+	// TODO invite code
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type GetFormsResponseDto []CreateUpdateFormResponseDto
+
+type GetFormDetailResponseDto struct {
+	Id          int64                      `json:"id"`
+	OwnerId     int64                      `json:"owner_id"`
+	Status      string                     `json:"status"`
+	CreatedById int64                      `json:"created_by_id"`
+	UpdatedById *int64                     `json:"updated_by_id"`
+	CreatedAt   string                     `json:"created_at"`
+	UpdatedAt   string                     `json:"updated_at"`
+	Content     models.FormContent         `json:"content"`
+	Meta        GetFormMetaDataResponseDto `json:"meta"`
 }
