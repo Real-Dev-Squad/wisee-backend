03bb9fb1776feb81b0d32e308f55ee80f11e71a6
yash raj <yashraj0207@outlook.com>
2024-03-17 17:26:25 +0530

    add logger package

--- src/routes/main.go
+++ src/routes/main.go
@@ -9,6 +9,9 @@ import (
 func SetupV1Routes(db *bun.DB) *gin.Engine {
 	var router = gin.Default()
 
+	// TODO: Configure CORS properly to allow only access from certain origins
+	router.Use(cors.Default())
+
 	v1 := router.Group("v1/")
 	UserRoutes(v1, db)
 	AuthRoutes(v1, db)
@@ -26,7 +29,5 @@ func SetupV1Routes(db *bun.DB) *gin.Engine {
 func Listen(listenAddress string, db *bun.DB) {
 	router := SetupV1Routes(db)
 
-	// TODO: Configure CORS properly to allow only access from certain origins
-	router.Use(cors.Default())
 	router.Run(listenAddress)
 }
