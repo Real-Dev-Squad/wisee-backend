8928a0d357266cd92ce1164f3a9087b896c5fedf
yash raj <yashraj0207@outlook.com>
2024-05-17 02:11:41 +0530

    drop index in shareable id down migration

--- database/migrations/000003_shareable_id.up.sql
+++ database/migrations/000003_shareable_id.up.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
 ALTER TABLE forms.form ADD COLUMN shareable_id VARCHAR(255) UNIQUE NOT NULL;
-CREATE UNIQUE INDEX sharable_id_idx ON forms.form (shareable_id);
+CREATE UNIQUE INDEX shareable_id_idx ON forms.form (shareable_id);
  
 COMMIT;
